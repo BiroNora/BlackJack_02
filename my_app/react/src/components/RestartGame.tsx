d6ef671c0a2854bd5c64392d5c31e67a5161e7ab
BiroNora <52859737+BiroNora@users.noreply.github.com>
2026-02-20 18:09:28 +0100

    Reloading css wip

--- my_app/react/src/components/RestartGame.tsx
+++ my_app/react/src/components/RestartGame.tsx
@@ -3,16 +3,20 @@ import "../styles/loading.css";
 export function Restart() {
   return (
     <div className="loading-container-centered">
-      <h1>W E L L C O M E  B A C K</h1>
-      <div className="bank merriweather">Your New Tokens: 1000</div>
-      <div className="bank merriweather">
-        <span className="grey-suit star-right">&#9733;&#9733;&#9733;</span>
-        <span className="red-suit gap-right">♥</span>
-        <span className="black-suit gap-right">♠</span>
-        <div >Enjoy Your Game</div>
-        <span className="black-suit gap-left">♣</span>
-        <span className="red-suit gap-left">♦</span>
-        <span className="grey-suit star-left">&#9733;&#9733;&#9733;</span>
+      <div>
+        <h1>W E L L C O M E B A C K</h1>
+        <div className="bank3 merriweather">Your New Tokens: 1000</div>
+        <div>
+          <div className="bank2 merriweather">
+            <span className="grey-suit star-right">&#9733;&#9733;&#9733;</span>
+            <span className="red-suit gap-right">♥</span>
+            <span className="black-suit gap-right">♠</span>
+            <div>Enjoy Your Game</div>
+            <span className="black-suit gap-left">♣</span>
+            <span className="red-suit gap-left">♦</span>
+            <span className="grey-suit star-left">&#9733;&#9733;&#9733;</span>
+          </div>
+        </div>
       </div>
     </div>
   );
